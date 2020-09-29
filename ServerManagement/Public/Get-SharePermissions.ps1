9a8c5c54e23306c690422b03cb81ab8a36160820
Trent Willingham <TrentWillingham@gmail.com>
2020-09-29 12:22:53 -0400

    share permissions

---
+++ ServerManagement/Public/Get-SharePermissions.ps1
@@ -0,0 +1,27 @@
+﻿function Get-SharePermission {
+    [CmdletBinding()]
+    [OutputType('Win32_SecurityDescriptor')]
+    param (
+        [Parameter(Mandatory)]
+        [string] $ShareName,
+
+        [Parameter()]
+        [string] $ComputerName = $env:COMPUTERNAME
+    )
+
+    begin {
+
+    }
+
+    process {
+        $ShareSecuritySettings = Get-CimInstance -ComputerName $ComputerName -ClassName 'Win32_LogicalShareSecuritySetting'
+        $ShareSecurity = $ShareSecuritySettings | Where-Object { $_.Name -eq $ShareName }
+        if ($ShareSecurity) {
+            (Invoke-CimMethod -InputObject $ShareSecurity -MethodName 'GetSecurityDescriptor').Descriptor
+        }
+    }
+
+    end {
+
+    }
+}
---
+++ ServerManagement/Public/Set-SharePermission.ps1
@@ -0,0 +1,29 @@
+﻿function Set-SharePermission {
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $ShareName,
+
+        [Parameter(Mandatory = $true,
+            ValueFromPipeline = $true)]
+        [object] $SecurityDescriptor,
+
+        [Parameter()]
+        [string] $ComputerName = $env:COMPUTERNAME
+    )
+
+    begin {
+
+    }
+
+    process {
+        $ShareSecuritySettings = Get-CIMInstance -ComputerName $ComputerName -ClassName 'Win32_LogicalShareSecuritySetting'
+        $ShareSecurity = $ShareSecuritySettings | Where-Object { $_.Name -eq $ShareName }
+        $Response = Invoke-CimMethod -InputObject $ShareSecurity -MethodName 'SetSecurityDescriptor' -Arguments @{Descriptor = $SecurityDescriptor}
+        $Response
+    }
+
+    end {
+
+    }
+}
