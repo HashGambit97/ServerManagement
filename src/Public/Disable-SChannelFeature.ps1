7b316ca312bd0a8d3c7187f1f9c45a17a98b5102
Trent Willingham <TrentWillingham@gmail.com>
2026-03-23 07:29:52 -0400

    Added support for TLS1.0 and TLS1.1 , fixed typos and improve comments in Disable-SChannelFeature function

--- src/Public/Disable-SChannelFeature.ps1
+++ src/Public/Disable-SChannelFeature.ps1
@@ -1,7 +1,8 @@
-function Disable-SChannelFeature {
+function Disable-SChannelFeature
+{
     <#
     .SYNOPSIS
-        Disable SChannel featuers on one or more computers.
+        Disable server side SChannel features on one or more computers.
     .DESCRIPTION
         The Disable-SChannelFeature cmdlet disables features in the SChannel security suite on Windows computers.  This cmdlet can be used to disable ciphers, key exchanges, and protocols that are consider insecure.
     .EXAMPLE
@@ -15,7 +16,7 @@ function Disable-SChannelFeature {
         http://psservermanagement.readthedocs.io/en/latest/functions/Disable-SChannelFeature
     .NOTES
         Author: Trent Willingham
-        Check out my other projects on GitHub https://github.com/twillin912
+        Check out my other projects on GitHub https://github.com/HashGambit97
     #>
     [CmdletBinding(
         SupportsShouldProcess
@@ -37,7 +38,7 @@ function Disable-SChannelFeature {
         [Parameter()]
         [switch]$3Des,
 
-        # Disables SChannel Diffe-Hellman key exchange.
+        # Disables SChannel Diffie-Hellman key exchange.
         [Parameter()]
         [switch]$Dhe,
 
@@ -51,48 +52,83 @@ function Disable-SChannelFeature {
 
         # Disables SChannel SSL v3 protocol usage.
         [Parameter()]
-        [switch]$Ssl3
+        [switch]$Ssl3,
 
+        # Disables SChannel TLS v1.0 protocol usage.
+        [Parameter()]
+        [switch]$Tls1,
+
+        # Disables SChannel TLS v1.1 protocol usage.
+        [Parameter()]
+        [switch]$Tls11
     )
 
-    begin {
+    begin
+    {
         $SChannelKey = 'SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL'
-        $Keys = @()
-        if ($3Des -or $All) {
-            $Keys += "$SChannelKey\Ciphers\Triple DES 168"
+        $KeysToDisable = @()
+        if ($Rc4 -or $All)
+        {
+            Write-Verbose "Adding RC4 cipher to list of features to disable."
+            $KeysToDisable += "$SChannelKey\Ciphers\RC4 40/128"
+            $KeysToDisable += "$SChannelKey\Ciphers\RC4 56/128"
+            $KeysToDisable += "$SChannelKey\Ciphers\RC4 128/128"
         }
-        if ($Dhe -or $All) {
-            $Keys += "$SChannelKey\KeyExchangeAlgorithms\Diffie-Hellman"
+        if ($3Des -or $All)
+        {
+            Write-Verbose "Adding 3DES cipher to list of features to disable."
+            $KeysToDisable += "$SChannelKey\Ciphers\Triple DES 168"
         }
-        if ($Rc4 -or $All) {
-            $Keys += "$SChannelKey\Ciphers\RC4 40/128"
-            $Keys += "$SChannelKey\Ciphers\RC4 56/128"
-            $Keys += "$SChannelKey\Ciphers\RC4 128/128"
+        if ($Dhe -or $All)
+        {
+            Write-Verbose "Adding Diffie-Hellman key exchange to list of features to disable."
+            $KeysToDisable += "$SChannelKey\KeyExchangeAlgorithms\Diffie-Hellman"
         }
-        if ($Ssl2 -or $All) {
-            $Keys += "$SChannelKey\Protocols\SSL 2.0\Server"
+        if ($Ssl2 -or $All)
+        {
+            Write-Verbose "Adding SSL v2 protocol to list of features to disable."
+            $KeysToDisable += "$SChannelKey\Protocols\SSL 2.0\Server"
         }
-        if ($Ssl3 -or $All) {
-            $Keys += "$SChannelKey\Protocols\SSL 3.0\Server"
+        if ($Ssl3 -or $All)
+        {
+            Write-Verbose "Adding SSL v3 protocol to list of features to disable."
+            $KeysToDisable += "$SChannelKey\Protocols\SSL 3.0\Server"
+        }
+        if ($Tls1 -or $All)
+        {
+            Write-Verbose "Adding TLS v1.0 protocol to list of features to disable."
+            $KeysToDisable += "$SChannelKey\Protocols\TLS 1.0\Server"
+        }
+        if ($Tls11 -or $All)
+        {
+            Write-Verbose "Adding TLS v1.1 protocol to list of features to disable."
+            $KeysToDisable += "$SChannelKey\Protocols\TLS 1.1\Server"
         }
     }
 
-    process {
-        foreach ($Computer in $ComputerName) {
-            if (!(Test-Connection -ComputerName $Computer -Count 1 -Quiet)) {
+    process
+    {
+        foreach ($Computer in $ComputerName)
+        {
+            if (!(Test-Connection -ComputerName $Computer -Count 1 -Quiet))
+            {
                 throw "Cannot connect to computer '$Computer', because it is offline."
             }
 
-            if ($PSCmdlet.ShouldProcess($Computer)) {
-                try {
+            if ($PSCmdlet.ShouldProcess($Computer))
+            {
+                try
+                {
                     $RemoteReg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', "$Computer")
-                    foreach ($Key in $Keys) {
+                    foreach ($Key in $KeysToDisable)
+                    {
                         Write-Debug -Message "Update Registry Key: $Key"
                         $RemoteKey = $RemoteReg.CreateSubKey("$Key", $true)
                         $RemoteKey.SetValue('Enabled', 0, 'DWord')
                     }
                 }
-                catch {
+                catch
+                {
                     Write-Error "Failed to update registry on '$Computer'.`n$_"
                     continue
                 }
@@ -100,7 +136,8 @@ function Disable-SChannelFeature {
         }
     }
 
-    end {
+    end
+    {
 
     }
 }
